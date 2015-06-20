8992d642db33458fa56379f9002e17c8808f45bd
Tony Narlock <tony@git-pull.com>
2015-06-20 04:25:47 -0500

    Add django_docutils code.

---
+++ django_docutils/engines.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+import io
+
+from django.conf import settings
+from django.template.base import TemplateDoesNotExist
+from django.template.backends.base import BaseEngine
+from django.template.backends.utils import csrf_input_lazy, csrf_token_lazy
+from django.template.engine import Engine
+
+from docutils import core
+
+from .directives import register_pygments_directive
+
+
+class Docutils(BaseEngine):
+    app_dirname = 'templates'
+
+    def __init__(self, params):
+        params = params.copy()
+        self.options = params.pop('OPTIONS').copy()
+        self.options.setdefault('debug', settings.DEBUG)
+        self.options.setdefault('file_charset', settings.FILE_CHARSET)
+        super(Docutils, self).__init__(params)
+        self.engine = Engine(self.dirs, self.app_dirs, **self.options)
+
+    def from_string(self, template_code):
+        return DocutilsTemplate(template_code, self.options)
+
+    def get_template(self, template_name):
+        for template_file in self.iter_template_filenames(template_name):
+            try:
+                with io.open(template_file, encoding=settings.FILE_CHARSET) as fp:
+                    template_code = fp.read()
+            except IOError:
+                continue
+
+            return DocutilsTemplate(template_code, self.options)
+        else:
+            raise TemplateDoesNotExist(template_name)
+
+
+class DocutilsTemplate(object):
+
+    def __init__(self, source, options):
+        self.source = source
+        self.options = options
+
+    def render(self, context=None, request=None):
+        context = self.options
+        if request is not None:
+            context['request'] = request
+            context['csrf_input'] = csrf_input_lazy(request)
+            context['csrf_token'] = csrf_token_lazy(request)
+        context = {
+            'source': self.source,
+            'writer_name': 'html-base'
+        }
+
+        return core.publish_parts(**context)['html_body']
+
+
+register_pygments_directive()
---
+++ django_docutils/views.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+from django.core.exceptions import ImproperlyConfigured
+
+from django.views.generic.base import TemplateView
+from django.template.response import TemplateResponse
+from django.template.loader import select_template
+
+class DocutilsResponse(TemplateResponse):
+
+    template_name = 'base.html'
+
+    def __init__(self, request, template, rst, context=None, content_type=None,
+            status=None, charset=None, using=None):
+        self.rst_name = rst
+        super(DocutilsResponse, self).__init__(
+            request, template, context, content_type, status, charset, using)
+
+    @property
+    def rendered_content(self):
+        """Return the freshly rendered content for the template and context
+        described by the TemplateResponse.
+
+        This *does not* set the final content of the response. To set the
+        response content, you must either call render(), or set the
+        content explicitly using the value of this property.
+        """
+
+        context = self._resolve_context(self.context_data)
+
+        # we should be able to use the engine to .Render this
+        from django.utils.safestring import mark_safe
+        context['content'] = mark_safe(select_template(self.rst_name, using='docutils').render())
+
+        template = self._resolve_template(self.template_name)
+        content = template.render(context, self._request)
+        return content
+
+
+class DocutilsView(TemplateView):
+    response_class = DocutilsResponse
+    rst_name = None
+
+    def render_to_response(self, context, **response_kwargs):
+        """Override to pay in rst content."""
+        return self.response_class(
+            request=self.request,
+            template=self.get_template_names(),
+            rst=self.get_rst_names(),
+            context=context,
+            using=self.template_engine,
+            **response_kwargs
+        )
+
+    def get_rst_names(self):
+        """
+        Follows after get_template_names, but for scanning for rst content.
+        """
+        if self.rst_name is None:
+            raise ImproperlyConfigured(
+                "DocutilsView requires either a definition of "
+                "'rst_name' or an implementation of 'get_rst_names()'")
+        else:
+            return [self.rst_name]
