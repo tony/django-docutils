3965bfa909a8365ed6ce172eca52de5ea50f4d62
Tony Narlock <tony@git-pull.com>
2015-06-20 05:09:18 -0500

    docs/conf.py remove unused sphinx-arg

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
-    'sphinxarg.ext',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
