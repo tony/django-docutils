34294ad92ae96fa301cdb68e431e8c2fae49a916
Tony Narlock <tony@git-pull.com>
2017-02-12 11:32:37 -0600

    tag 0.3.4

--- django_docutils/__about__.py
+++ django_docutils/__about__.py
@@ -1,7 +1,7 @@
 __title__ = 'django-docutils'
 __package_name__ = 'django_docutils'
 __description__ = 'Documentation Utilities (Docutils, reStructuredText) for django.'
-__version__ = '0.3.3'
+__version__ = '0.3.4'
 __author__ = 'Tony Narlock'
 __email__ = 'tony@git-pull.com'
 __license__ = 'BSD'
