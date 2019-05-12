33bd0ed7af2fcb888cbb1feef90b4f4876fb9f6f
Tony Narlock <tony@git-pull.com>
2019-05-12 11:49:47 -0500

    Add sphinx-issues

--- doc/conf.py
+++ doc/conf.py
@@ -16,12 +16,15 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'alagitpull',
+    'sphinx_issues',
 ]
 
 releases_unstable_prehistory = True
 releases_document_name = ["history"]
 releases_issue_uri = "https://github.com/tony/django-docutils/issues/%s"
 releases_release_uri = "https://github.com/tony/django-docutils/tree/v%s"
+
+issues_github_path = about['__github__']
 
 templates_path = ['_templates']
 
