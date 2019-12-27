0844a0fa39ca8589584bf43aa9808513170ce3b2
Tony Narlock <tony@git-pull.com>
2019-12-27 09:13:02 -0600

    Add tests for find_app_configs... changes

--- django_docutils/lib/fixtures/tests/test_utils.py
+++ django_docutils/lib/fixtures/tests/test_utils.py
@@ -1,11 +1,25 @@
+import py
+import pytest
+
 from django.apps import apps
 
+from django_docutils.lib.fixtures.tests.conftest import create_bare_app
 from django_docutils.lib.fixtures.utils import (
     find_app_configs_with_fixtures,
     find_rst_files,
     find_rst_files_in_app,
     get_model_from_post_app,
 )
+
+
+@pytest.fixture(scope='function')
+def bare_app_config_with_empty_fixture_dir(tmpdir_factory, request, settings):
+    tmpdir = tmpdir_factory.mktemp('bare_project')
+
+    app_config = create_bare_app(tmpdir, request, settings, 'app_with_empty_fixtures')
+    sample_app_dir = py.path.local(app_config.path)
+    sample_app_dir.ensure('fixtures', dir=True)
+    yield app_config
 
 
 def test_find_rst_files(tmpdir):
@@ -19,7 +33,24 @@ def test_find_rst_files(tmpdir):
 
 
 def test_find_app_configs_with_fixtures(sample_app_config):
-    assert sample_app_config in find_app_configs_with_fixtures()
+    assert sample_app_config in find_app_configs_with_fixtures(has_rst_files=False)
+
+
+def test_find_app_configs_with_fixtures_with_rst_files(bare_app_config):
+    assert bare_app_config not in find_app_configs_with_fixtures()
+    assert bare_app_config not in find_app_configs_with_fixtures(has_rst_files=False)
+    assert bare_app_config not in find_app_configs_with_fixtures(has_rst_files=True)
+
+
+def test_find_app_configs_with_fixtures_with_empty_fixtures_dir(
+    bare_app_config_with_empty_fixture_dir,
+):
+    assert bare_app_config_with_empty_fixture_dir in find_app_configs_with_fixtures(
+        has_rst_files=False
+    )
+    assert bare_app_config_with_empty_fixture_dir not in find_app_configs_with_fixtures(
+        has_rst_files=True
+    )
 
 
 def test_find_rst_files_in_app(sample_app_config):
