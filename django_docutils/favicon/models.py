7dcb3d06551d7bac84e90e7621b5b8f9cbf8f0ce
Tony Narlock <tony@git-pull.com>
2019-12-26 23:31:25 -0600

    Add get_favicon_model

--- django_docutils/favicon/models.py
+++ django_docutils/favicon/models.py
@@ -1,5 +1,25 @@
+from django.apps import apps as django_apps
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+
+def get_favicon_model():
+    """
+    Return the Favicon model that is active in this project.
+    """
+    try:
+        return django_apps.get_model(settings.FAVICON_MODEL, require_ready=False)
+    except ValueError:
+        raise ImproperlyConfigured(
+            "FAVICON_MODEL must be of the form 'app_label.model_name'"
+        )
+    except LookupError:
+        raise ImproperlyConfigured(
+            "FAVICON_MODEL refers to model '%s' that has not been installed"
+            % settings.FAVICON_MODEL
+        )
 
 
 class FaviconBase(models.Model):
