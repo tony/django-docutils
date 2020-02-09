452c32ef11ae6470f71dce534bcb6ee6809298c8
Tony Narlock <tony@git-pull.com>
2020-02-09 12:43:40 -0600

    Remove print statements

--- django_docutils/lib/transforms/font_awesome.py
+++ django_docutils/lib/transforms/font_awesome.py
@@ -7,6 +7,7 @@ from django_docutils.lib.settings import BASED_LIB_RST
 
 url_patterns = BASED_LIB_RST.get('font_awesome', {}).get('url_patterns', {})
 
+
 class InjectFontAwesome(Transform):
 
     default_priority = 680
@@ -15,13 +16,11 @@ class InjectFontAwesome(Transform):
         for target in self.document.traverse(nodes.reference):
             if target.hasattr('refuri'):
                 url = target['refuri']
-                print(url_patterns)
                 for url_pattern, classes in url_patterns.items():
                     if re.match(url_pattern, url):
 
                         fa_tag = f'<em class="{classes}"></em>'
                         title = utils.unescape(target[0])
-                        print(target, url, fa_tag,title)
                         rn = nodes.reference('', '', internal=True, refuri=url)
                         rn += nodes.raw('', fa_tag, format='html')
                         rn += nodes.Text(title, title)
