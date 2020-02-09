55584975d4af8c751a6f93bbadf7ea4b4279a960
Tony Narlock <tony@git-pull.com>
2020-02-09 11:48:08 -0600

    Example font_awesome settings

--- django_docutils/lib/settings.py
+++ django_docutils/lib/settings.py
@@ -1,3 +1,13 @@
 from django.conf import settings
 
-BASED_LIB_RST = getattr(settings, 'BASED_LIB_RST', {})
+BASED_LIB_RST = getattr(
+    settings,
+    'BASED_LIB_RST',
+    {
+        "font_awesome": {
+            "url_patterns": {
+                r'.*twitter.com.*': 'fab fa-twitter',
+            }
+        }
+    },
+)
