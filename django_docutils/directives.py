ace2c50a5e69f8d451dbe4c03d07a6d2ce7519dc
Tony Narlock <tony@git-pull.com>
2020-08-08 07:41:57 -0500

    docs(django_docutils): formatting fixes

--- django_docutils/directives.py
+++ django_docutils/directives.py
@@ -9,39 +9,39 @@ from pygments.lexers import TextLexer, get_lexer_by_name
 
 # -*- coding: utf-8 -*-
 """
-    The Pygments reStructuredText directive
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+The Pygments reStructuredText directive
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    This fragment is a Docutils_ 0.5 directive that renders source code
-    (to HTML only, currently) via Pygments.
+This fragment is a Docutils_ 0.5 directive that renders source code
+(to HTML only, currently) via Pygments.
 
-    To use it, adjust the options below and copy the code into a module
-    that you import on initialization.  The code then automatically
-    registers a ``sourcecode`` directive that you can use instead of
-    normal code blocks like this::
+To use it, adjust the options below and copy the code into a module
+that you import on initialization.  The code then automatically
+registers a ``sourcecode`` directive that you can use instead of
+normal code blocks like this::
 
-        .. sourcecode:: python
+    .. sourcecode:: python
 
-            My code goes here.
+        My code goes here.
 
-    If you want to have different code styles, e.g. one with line numbers
-    and one without, add formatters with their names in the VARIANTS dict
-    below.  You can invoke them instead of the DEFAULT one by using a
-    directive option::
+If you want to have different code styles, e.g. one with line numbers
+and one without, add formatters with their names in the VARIANTS dict
+below.  You can invoke them instead of the DEFAULT one by using a
+directive option::
 
-        .. sourcecode:: python
-            :linenos:
+    .. sourcecode:: python
+        :linenos:
 
-            My code goes here.
+        My code goes here.
 
-    Look at the `directive documentation`_ to get all the gory details.
+Look at the `directive documentation`_ to get all the gory details.
 
-    .. _Docutils: http://docutils.sf.net/
-    .. _directive documentation:
-       http://docutils.sourceforge.net/docs/howto/rst-directives.html
+.. _Docutils: http://docutils.sf.net/
+.. _directive documentation:
+   http://docutils.sourceforge.net/docs/howto/rst-directives.html
 
-    :copyright: Copyright 2006-2015 by the Pygments team, see AUTHORS.
-    :license: BSD, see LICENSE for details.
+:copyright: Copyright 2006-2015 by the Pygments team, see AUTHORS.
+:license: BSD, see LICENSE for details.
 """
 
 # Options
@@ -51,18 +51,17 @@ from pygments.lexers import TextLexer, get_lexer_by_name
 INLINESTYLES = False
 
 
-# The default formatter
+#: The default formatter
 DEFAULT = HtmlFormatter(noclasses=INLINESTYLES)
 
-# Add name -> formatter pairs for every variant you want to use
+#: Add name -> formatter pairs for every variant you want to use
 VARIANTS = {
     # 'linenos': HtmlFormatter(noclasses=INLINESTYLES, linenos=True),
 }
 
 
 class CodeBlock(Directive):
-    """ Source code syntax hightlighting.
-    """
+    """Source code syntax hightlighting."""
 
     required_arguments = 1
     optional_arguments = 0
@@ -86,13 +85,17 @@ class CodeBlock(Directive):
 def register_pygments_directive(directive='code-block'):
     """Register pygments directive.
 
-    :param directive: directive name to register pygments to.
+    Parameters
+    ----------
+    directive : str
+        directive name to register pygments to.
 
-        If you wish to use (override) code-block (default), that means::
+    Examples
+    --------
+    If you wish to use (override) code-block (default), that means::
 
-            .. code-block::
+        .. code-block::
 
-                // will be highlighted by pygments
-    :type directive: string
+            // will be highlighted by pygments
     """
     directives.register_directive(directive, CodeBlock)
