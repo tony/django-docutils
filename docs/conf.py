f2d693524e18114562985ba6651fb1f4d0926362
Tony Narlock <tony@git-pull.com>
2020-08-09 15:06:35 -0500

    docs: Fix gh link path

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ extensions = [
     'sphinx_issues',
 ]
 
-issues_github_path = about['__github__']
+issues_github_path = about['__github__'].replace('https://github.com/', '')
 
 templates_path = ['_templates']
 
