4e60ff7d1396bedda54d697dbed48f858cbb4574
Tony Narlock <tony@git-pull.com>
2022-08-16 13:06:19 +0200

    tests(settings): Add rst_model for django-stubs

--- tests/settings.py
+++ tests/settings.py
@@ -2,6 +2,6 @@ SECRET_KEY = "dummy"
 
 DATABASES = {"default": {"NAME": ":memory:", "ENGINE": "django.db.backends.sqlite3"}}
 
-INSTALLED_APPS = []
+INSTALLED_APPS = ["django.contrib.contenttypes", "django_docutils.rst_post"]
 
 USE_TZ = False
