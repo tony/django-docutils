0bc9725c460833c189213dafb06e900cf24f8ceb
Tony Narlock <tony@git-pull.com>
2022-09-25 07:19:28 -0500

    docs: Default autodoc member options

--- docs/conf.py
+++ docs/conf.py
@@ -100,6 +100,13 @@ issue_url_tpl = "https://github.com/tony/django-docutils/issues/{issue_id}"
 # sphinx.ext.autodoc
 autodoc_mock_imports = ["django"]
 toc_object_entries_show_parents = "hide"
+autodoc_default_options = {
+    "undoc-members": True,
+    "members": True,
+    "private-members": True,
+    "show-inheritance": True,
+    "member-order": "bysource",
+}
 
 # sphinxext.opengraph
 ogp_site_url = about["__docs__"]
