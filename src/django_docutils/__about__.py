e9c4704f61ff213d38618b39997915b98bf7f8c0
Tony Narlock <tony@git-pull.com>
2023-09-10 10:45:19 -0500

    Tag v0.13.1 (ci and test improvements)

--- src/django_docutils/__about__.py
+++ src/django_docutils/__about__.py
@@ -1,7 +1,7 @@
 __title__ = "django-docutils"
 __package_name__ = "django_docutils"
 __description__ = "Documentation Utilities (Docutils, reStructuredText) for django."
-__version__ = "0.13.0"
+__version__ = "0.13.1"
 __author__ = "Tony Narlock"
 __github__ = "https://github.com/tony/django-docutils"
 __pypi__ = "https://pypi.org/project/django-docutils/"
