1629267de683ef9903960b87bf1ce9f9f43cb728
Tony Narlock <tony@git-pull.com>
2023-09-10 08:45:30 -0500

    Tag v0.12.0 (remove ORM-based parts via #374)

--- src/django_docutils/__about__.py
+++ src/django_docutils/__about__.py
@@ -1,7 +1,7 @@
 __title__ = "django-docutils"
 __package_name__ = "django_docutils"
 __description__ = "Documentation Utilities (Docutils, reStructuredText) for django."
-__version__ = "0.11.0.post0"
+__version__ = "0.12.0"
 __author__ = "Tony Narlock"
 __github__ = "https://github.com/tony/django-docutils"
 __pypi__ = "https://pypi.org/project/django-docutils/"
