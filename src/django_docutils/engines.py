ab7d1ef9a563731c734d9c5fc16c7757a2b66db7
Tony Narlock <tony@git-pull.com>
2023-09-24 07:33:05 -0500

    chore(mypy): Add more typings for engines

--- src/django_docutils/engines.py
+++ src/django_docutils/engines.py
@@ -1,4 +1,7 @@
+import typing as t
+
 from django.conf import settings
+from django.http.request import HttpRequest
 from django.template.backends.base import BaseEngine
 from django.template.backends.utils import csrf_input_lazy, csrf_token_lazy
 from django.template.engine import Engine
@@ -11,17 +14,17 @@ from .directives import register_pygments_directive
 class Docutils(BaseEngine):
     app_dirname = "templates"
 
-    def __init__(self, params):
+    def __init__(self, params: t.Dict[str, t.Any]) -> None:
         params = params.copy()
         self.options = params.pop("OPTIONS").copy()
         self.options.setdefault("debug", settings.DEBUG)
         super().__init__(params)
         self.engine = Engine(self.dirs, self.app_dirs, **self.options)
 
-    def from_string(self, template_code):
+    def from_string(self, template_code: str) -> "DocutilsTemplate":
         return DocutilsTemplate(template_code, self.options)
 
-    def get_template(self, template_name):
+    def get_template(self, template_name: str) -> "DocutilsTemplate":
         for template_file in self.iter_template_filenames(template_name):
             try:
                 with open(template_file, encoding="utf-8") as fp:
@@ -35,11 +38,15 @@ class Docutils(BaseEngine):
 
 
 class DocutilsTemplate:
-    def __init__(self, source, options):
+    def __init__(self, source: str, options: t.Dict[str, t.Any]) -> None:
         self.source = source
         self.options = options
 
-    def render(self, context=None, request=None):
+    def render(
+        self,
+        context: t.Optional[t.Dict[str, t.Any]] = None,
+        request: t.Optional[HttpRequest] = None,
+    ) -> str:
         context = self.options
         if request is not None:
             context["request"] = request
@@ -47,7 +54,7 @@ class DocutilsTemplate:
             context["csrf_token"] = csrf_token_lazy(request)
         context = {"source": self.source, "writer_name": "html"}
 
-        return core.publish_parts(**context)["html_body"]
+        return t.cast(str, core.publish_parts(**context)["html_body"])
 
 
 register_pygments_directive()
