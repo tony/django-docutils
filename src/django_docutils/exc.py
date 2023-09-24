3a759cdd1c49355298a1b71176c6de5e8a6dc3c3
Tony Narlock <tony@git-pull.com>
2023-09-24 04:16:26 -0500

    feat(exc): Add DocutilsNotInstalled

--- src/django_docutils/exc.py
+++ src/django_docutils/exc.py
@@ -1,2 +1,12 @@
 class DjangoDocutilsException(Exception):
     pass
+
+
+class DocutilsNotInstalled(DjangoDocutilsException):
+    def __init__(
+        self,
+        message: str = "The Python docutils library isn't installed",
+        *args: object,
+        **kwargs: object
+    ) -> None:
+        return super().__init__(message, *args, **kwargs)
