4925bb2a40d4d9d7b7279eb727d126ee8c5ecbcc
Tony Narlock <tony@git-pull.com>
2023-09-30 10:54:21 -0500

    chore(mypy): Typings for roles/twitter

--- src/django_docutils/lib/roles/twitter.py
+++ src/django_docutils/lib/roles/twitter.py
@@ -1,7 +1,20 @@
+import typing as t
+
+from docutils.parsers.rst.states import Inliner
+
 from .common import generic_url_role
+from .types import GenericUrlRoleFn
 
 
-def twitter_role(name, rawtext, text, lineno, inliner, options=None, content=None):
+def twitter_role(
+    name: str,
+    rawtext: str,
+    text: str,
+    lineno: int,
+    inliner: Inliner,
+    options: t.Optional[t.Dict[str, t.Any]] = None,
+    content: t.Optional[str] = None,
+) -> GenericUrlRoleFn:
     """Role for linking to twitter articles.
 
     :twitter:`@username` ->
@@ -12,13 +25,10 @@ def twitter_role(name, rawtext, text, lineno, inliner, options=None, content=Non
        link: https://twitter.com/username
        text: follow on me on twitter
     """
-
-    if content is None:
-        content = []
     if options is None:
         options = {}
 
-    def url_handler(target):
+    def url_handler(target: str) -> str:
         if "@" in target:
             target = target.replace("@", "")
 
