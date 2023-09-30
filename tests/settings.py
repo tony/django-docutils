b121099f12957e717b696852a2c5176e4c0fbaa1
Tony Narlock <tony@git-pull.com>
2023-09-30 07:06:48 -0500

    chore(mypy): Add test/settings.py for typings

--- tests/settings.py
+++ tests/settings.py
@@ -1,6 +1,10 @@
+import typing as t
+
 SECRET_KEY = "dummy"
 
 DATABASES = {"default": {"NAME": ":memory:", "ENGINE": "django.db.backends.sqlite3"}}
+
+DJANGO_DOCUTILS_LIB_TEXT: t.Dict[str, t.Dict[str, str]] = {}
 
 INSTALLED_APPS = ["django.contrib.contenttypes"]
 
