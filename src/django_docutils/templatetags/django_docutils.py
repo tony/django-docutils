7be5b50cafb4729d77ce98c1cc319bdb866b24dc
Tony Narlock <tony@git-pull.com>
2023-09-30 15:30:03 -0500

    chore(mypy): Fix subclass Any issue by using relative import

--- src/django_docutils/templatetags/django_docutils.py
+++ src/django_docutils/templatetags/django_docutils.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.utils.encoding import force_bytes, force_str
 from django.utils.safestring import mark_safe
 
-from django_docutils.exc import DocutilsNotInstalled
+from ..exc import DocutilsNotInstalled
 
 register = template.Library()
 
