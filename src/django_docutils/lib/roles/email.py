dbfd8d1b0a21657db439520d6f8564103992a49f
Tony Narlock <tony@git-pull.com>
2023-09-30 10:56:54 -0500

    chore(mypy): Typings for roles/email

--- src/django_docutils/lib/roles/email.py
+++ src/django_docutils/lib/roles/email.py
@@ -1,7 +1,20 @@
+import typing as t
+
+from docutils.parsers.rst.states import Inliner
+
 from .common import generic_url_role
+from .types import GenericUrlRoleFn
 
 
-def email_role(name, rawtext, text, lineno, inliner, options=None, content=None):
+def email_role(
+    name: str,
+    rawtext: str,
+    text: str,
+    lineno: int,
+    inliner: Inliner,
+    options: t.Optional[t.Dict[str, t.Any]] = None,
+    content: t.Optional[str] = None,
+) -> GenericUrlRoleFn:
     """Role for linking to email articles.
 
     :email:`me@localhost` ->
@@ -11,15 +24,11 @@ def email_role(name, rawtext, text, lineno, inliner, options=None, content=None)
     :email:`E-mail me <me@localhost>` ->
        link: mailto:me@localhost
        text: E-mail me
-
     """
-
-    if content is None:
-        content = []
     if options is None:
         options = {}
 
-    def url_handler(target):
+    def url_handler(target: str) -> str:
         return f"mailto:{target}"
 
     return generic_url_role(name, text, url_handler)
