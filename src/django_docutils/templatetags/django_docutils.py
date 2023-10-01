27f82ee47c0eababb2a7cddfafabc17765e1787c
Tony Narlock <tony@git-pull.com>
2023-10-01 11:03:49 -0500

    refactor!: Use restructuredtext_filter w/ ReStructuredTextNode

--- src/django_docutils/templatetags/django_docutils.py
+++ src/django_docutils/templatetags/django_docutils.py
@@ -1,37 +1,15 @@
 from django import template
-from django.conf import settings
-from django.utils.encoding import force_bytes, force_str
+from django.template.context import Context
+from django.utils.encoding import force_str
 from django.utils.safestring import mark_safe
 
-from ..exc import DocutilsNotInstalled
+from ..lib.templatetags.rst import ReStructuredTextNode
 
 register = template.Library()
 
 
-class ReStructuredTextLibraryMissingForDjangoFilter(
-    DocutilsNotInstalled, template.TemplateSyntaxError, ImportError
-):
-    def __init__(self, *args: object, **kwargs: object) -> None:
-        return super().__init__(
-            "Error in 'restructuredtext' filter: "
-            "The Python docutils library isn't installed",
-            *args,
-            **kwargs
-        )
-
-
 @register.filter(is_safe=True)
 def restructuredtext(value: str) -> str:
-    """RestructuredText Filter
-
-    Credit
-    ------
-    Thank you readthedocs.org.
-
-    Belated credit (2023-10-01), via GitHub Search, I see readthedocs.org, had this in
-    `8609c16 <https://github.com/readthedocs/readthedocs.org/commit/8609c16db5c7d92cb5272addab1910a6eddbb2f1>`_,
-    with an `MIT license <https://github.com/readthedocs/readthedocs.org/blob/8609c16db5c7d92cb5272addab1910a6eddbb2f1/LICENSE.mit>`_.
-    """
     import warnings
 
     warnings.warn(
@@ -39,20 +17,7 @@ def restructuredtext(value: str) -> str:
         category=DeprecationWarning,
         stacklevel=2,
     )
-    try:
-        from docutils.core import publish_parts
-    except ImportError as e:
-        if settings.DEBUG:
-            raise ReStructuredTextLibraryMissingForDjangoFilter() from e
-        return force_str(value)
-    else:
-        docutils_settings = getattr(settings, "RESTRUCTUREDTEXT_FILTER_SETTINGS", {})
-        parts = publish_parts(
-            source=force_bytes(value),
-            writer_name="html5_polyglot",
-            settings_overrides=docutils_settings,
-        )
 
-        assert isinstance(parts["fragment"], str)
-
-        return mark_safe(force_str(parts["fragment"]))
+    return mark_safe(
+        force_str(ReStructuredTextNode(value, [], {}, None).render(Context({})))
+    )
