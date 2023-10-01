ba599ca85d74bbfc2229855585357a15747dcb5e
Tony Narlock <tony@git-pull.com>
2023-10-01 08:15:44 -0500

    docs: Add credit for reStructuredText filter to readthedocs (MIT licensed)
    django-docutils was originally on a private codebase of mine. Despite me
    being meticulous when reusing permissively licensed code, I didn't credit
    the original author in the comments: @ericholscher.
    
    - https://github.com/readthedocs/readthedocs.org/commit/8609c16db5c7d92cb5272addab1910a6eddbb2f1
    - https://github.com/readthedocs/readthedocs.org/blob/8609c16db5c7d92cb5272addab1910a6eddbb2f1/LICENSE.mit

--- src/django_docutils/templatetags/django_docutils.py
+++ src/django_docutils/templatetags/django_docutils.py
@@ -22,6 +22,16 @@ class ReStructuredTextLibraryMissingForDjangoFilter(
 
 @register.filter(is_safe=True)
 def restructuredtext(value: str) -> str:
+    """RestructuredText Filter
+
+    Credit
+    ------
+    Thank you readthedocs.org.
+
+    Belated credit (2023-10-01), via GitHub Search, I see readthedocs.org, had this in
+    `8609c16 <https://github.com/readthedocs/readthedocs.org/commit/8609c16db5c7d92cb5272addab1910a6eddbb2f1>`_,
+    with an `MIT license <https://github.com/readthedocs/readthedocs.org/blob/8609c16db5c7d92cb5272addab1910a6eddbb2f1/LICENSE.mit>`_.
+    """
     import warnings
 
     warnings.warn(
