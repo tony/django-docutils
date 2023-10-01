63d60e44918e420761c5d06e6431e3d46cc84c78
Tony Narlock <tony@git-pull.com>
2023-10-01 13:30:44 -0500

    project: Update description

--- src/django_docutils/__about__.py
+++ src/django_docutils/__about__.py
@@ -1,6 +1,6 @@
 __title__ = "django-docutils"
 __package_name__ = "django_docutils"
-__description__ = "Documentation Utilities (Docutils, reStructuredText) for django."
+__description__ = "Docutils (a.k.a. reStructuredText, reST, RST) support for django."
 __version__ = "0.14.1"
 __author__ = "Tony Narlock"
 __github__ = "https://github.com/tony/django-docutils"
