f5fdaec1f4b21e7a72eb03e45a63a01911295a0c
Tony Narlock <tony@git-pull.com>
2023-10-07 14:52:19 -0500

    settings(lib): Add lib/settings.py

--- src/django_docutils/lib/settings.py
+++ src/django_docutils/lib/settings.py
@@ -1,7 +1,20 @@
+import typing as t
+
 from django.conf import settings
 
-DJANGO_DOCUTILS_LIB_RST = getattr(settings, "DJANGO_DOCUTILS_LIB_RST", {})
+if t.TYPE_CHECKING:
+    from .types import DjangoDocutilsLibRSTSettings, DjangoDocutilsLibTextSettings
 
-INJECT_FONT_AWESOME = (
+DJANGO_DOCUTILS_LIB_RST = t.cast(
+    "DjangoDocutilsLibRSTSettings", getattr(settings, "DJANGO_DOCUTILS_LIB_RST", {})
+)
+"""Settings for reStructuredText"""
+
+INJECT_FONT_AWESOME: bool = (
     DJANGO_DOCUTILS_LIB_RST.get("font_awesome", {}).get("url_patterns") is not None
+)
+
+DJANGO_DOCUTILS_LIB_TEXT = t.cast(
+    "DjangoDocutilsLibTextSettings",
+    getattr(settings, "DJANGO_DOCUTILS_LIB_TEXT", {"uncapitalized_word_filters": []}),
 )
