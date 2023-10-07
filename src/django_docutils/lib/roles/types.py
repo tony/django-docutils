7cc0ce4314256e8fcfe1d56e500e1076731fe5f7
Tony Narlock <tony@git-pull.com>
2023-10-07 08:59:06 -0500

    docs(role[types]): Add docstrings

--- src/django_docutils/lib/roles/types.py
+++ src/django_docutils/lib/roles/types.py
@@ -5,13 +5,32 @@ from typing_extensions import Protocol
 
 
 class UrlHandlerFn(Protocol):
+    """Role function that directly maps to a URL."""
+
     def __call__(self, target: str) -> str:
         ...
 
 
 class RemoteUrlHandlerFn(Protocol):
+    """Role function that can query an external source for its title."""
+
     def __call__(self, target: str) -> t.Tuple[str, str]:
         ...
 
 
 GenericUrlRoleFn = t.Tuple[t.List[nodes.reference], t.List[t.Any]]
+"""Role function return value.
+
+See also
+--------
+
+From `Docutils: How to: RST Roles
+<https://docutils.sourceforge.io/docs/howto/rst-roles.html>`_:
+
+    Role functions return a tuple of two values:
+    
+    - A list of nodes which will be inserted into the document tree at the point where
+      the interpreted role was encountered (can be an empty list).
+    - A list of system messages, which will be inserted into the document tree
+      immediately after the end of the current block (can also be empty).
+"""
