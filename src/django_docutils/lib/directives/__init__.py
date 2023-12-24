f2d2dd405422ad150d728b73d73197d57676fd36
Tony Narlock <tony@git-pull.com>
2023-12-23 19:53:16 -0600

    docs(register_django_docutils_directives): Clean up docstring

--- src/django_docutils/lib/directives/__init__.py
+++ src/django_docutils/lib/directives/__init__.py
@@ -6,17 +6,11 @@ from ..settings import DJANGO_DOCUTILS_LIB_RST
 
 
 def register_django_docutils_directives() -> None:
-    """Register all directives, exists to avoid race conditions.
+    """Register docutils directives.
 
-    Sometimes stuff like publish_parts can be ran from command line functions
-    tests. There's also ways we could avoid this by placing it in __init__
-    of django_docutils.lib, but that's a bit implicit. Investigate that later.
-
-    In order to make this work across django projects, let's use django
-    settings to register to them.
-
-    Why? Not all django projects want code highlighting (which requires
-    pygments). Let's use a TEMPLATES-style django config:
+    Examples
+    --------
+    Let's use a TEMPLATES-style django config:
 
     >>> DJANGO_DOCUTILS_LIB_RST = {
     ...    'directives': {  #: directive-name: Directive class (import string)
